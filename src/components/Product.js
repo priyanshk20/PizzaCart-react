edd180ff793c68ccd378b770fd24d7f96327f0f8
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-07-22 13:34:40 +0530

    till add to cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Product = () => {
+const Product = (props) => {
     return (
-        <div>
+      <Link to= {`/products/${props.product._id}`} >
+      <div>
              <div className="text-center">
-          <img src="/images/peproni.png" alt="peperoni" />
-          <h2 className="text-lg font-bold py-2">Havana Special</h2>
+          <img src={props.product.image} alt="peperoni" />
+          <h2 className="text-lg font-bold py-2">{props.product.name}</h2>
           <span className="bg-gray-200 rounded-full py-1 text-sm px-4">
-            Small
+            {props.product.size}
           </span>
           <div className="flex justify-between mt-4 items-center">
-            <span className="text-lg font-bold">$10</span>
+            <span className="text-lg font-bold">{props.product.price} ₹</span>
             <button className="py-1 px-4 rounded-full bg-yellow-500 font-bold">ADD</button>
           </div>
         </div>
         </div>
+      </Link>
+       
     )
 }
 
